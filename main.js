76f6a2fc039b14cccfda5243dadd51d2998504c2
MaysamTayser <130462290+MaysamTayser@users.noreply.github.com>
2024-06-06 21:44:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+ 
+ var change=document.querySelector(".fontSize");
+
+ 
+ var largF=document.querySelector(".larg");
+
+
+var smallF=document.querySelector(".smallBtn");
+console.log(change);
+console.log(largF);
+console.log(smallF);
+function changeFontL(){
+    change.classList.add("larg");
+    change.classList.remove("smallF");
+}
+function changeFontS(){
+    change.classList.add("smallF");
+    change.classList.remove("larg");
+}
+largF.onclick=changeFontL;
+smallF.onclick=changeFontS;
